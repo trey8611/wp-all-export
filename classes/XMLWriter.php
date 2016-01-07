a3212dd0b9a5af6a6f5ee4c5e2643c5712470e32
Max Tsiplyakov <makstsiplyakov@gmail.com>
2016-01-07 15:46:23 +0200

    initial commit

---
+++ classes/XMLWriter.php
@@ -0,0 +1,30 @@
+<?php
+
+class PMXE_XMLWriter extends XMLWriter
+{
+
+	public function putElement( $ns, $element, $uri, $value )
+	{
+		if (empty($ns))
+		{
+			return $this->writeElement( $element, $value );
+		}
+		else
+		{
+			return $this->writeElementNS( $ns, $element, $uri, $value );
+		}
+	}
+
+	public function beginElement($ns, $element, $uri)
+	{
+		if (empty($ns))
+		{
+			return $this->startElement( $element );
+		}
+		else
+		{
+			return $this->startElementNS( $ns, $element, $uri );
+		}
+	}
+
+} 
