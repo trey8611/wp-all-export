39710c1fb508649a16bf67fb4dd27bfd57835267
Joe Guilmette <joe.guilmette@gmail.com>
2018-08-26 16:52:42 +0700

    move tooltip https://github.com/soflyy/wp-all-export-pro/issues/367

--- src/Scheduling/views/ConnectionIcon.php
+++ src/Scheduling/views/ConnectionIcon.php
@@ -3,7 +3,7 @@ $scheduling = \Wpae\Scheduling\Scheduling::create();
 ?>
 <span class="wpai-no-license" <?php if ($scheduling->checkLicense()) { ?> style="display: none;" <?php } ?> >
 
-    <a href="#" style="z-index: 1000; position: absolute; top: 1px; left: 152px; padding: 0;" class="help_scheduling tipsy"
+    <a href="#" style="z-index: 1000; position: absolute; top: 1px; left: 155px; padding: 0;" class="help_scheduling tipsy"
        title="Automatic Scheduling is a paid service from Soflyy. Click for more info.">
         <img style="width: 16px; "
              src="<?php echo PMXE_ROOT_URL; ?>/static/img/s-question.png"/>
@@ -15,7 +15,7 @@ $scheduling = \Wpae\Scheduling\Scheduling::create();
     <?php if ( $scheduling->checkConnection() ) {
         ?>
         <a href="#" class="help_scheduling" title="Connection to WP All Export servers is stable and confirmed"
-              style="z-index: 1000; position: absolute; top: 1px; left: 152px; padding: 0; width: 16px;">
+              style="z-index: 1000; position: absolute; top: 1px; left: 155px; padding: 0; width: 16px;">
         <img src="<?php echo PMXE_ROOT_URL; ?>/static/img/s-check.png" style="width: 16px;"/>
     </a>
         <?php
